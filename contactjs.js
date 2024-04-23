2924ea9446a1efe9077e0c0e63d137e3d9c3c490
EaswerA <easwer567@gmail.com>
2024-04-23 10:06:00 +0530

    added files to repo

---
+++ contactjs.js
@@ -0,0 +1,29 @@
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+
+hamburger.addEventListener('click', () => {
+    //Animate Links
+    navLinks.classList.toggle("open");
+    links.forEach(link => {
+        link.classList.toggle("fade");
+    });
+
+    //Hamburger Animation
+    hamburger.classList.toggle("toggle");
+});
+
+document.getElementById('loginbutton').onclick = function () {
+    window.location.href = 'LoginPage.html';
+};
+
+document.getElementById("cards").onmousemove = e => {
+    for (const card of document.getElementsByClassName("card")) {
+        const rect = card.getBoundingClientRect(),
+            x = e.clientX - rect.left,
+            y = e.clientY - rect.top;
+
+        card.style.setProperty("--mouse-x", `${x}px`);
+        card.style.setProperty("--mouse-y", `${y}px`);
+    };
+}
