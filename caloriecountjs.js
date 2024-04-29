4954a29fa778855f4243d13c95bc37721ebc289c
EaswerA <easwer567@gmail.com>
2024-04-29 14:43:37 +0530

    added articles

--- caloriecountjs.js
+++ caloriecountjs.js
@@ -1,3 +1,22 @@
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+
+hamburger.addEventListener('click', () => {
+    //Animate Links
+    navLinks.classList.toggle("open");
+    links.forEach(link => {
+        link.classList.toggle("fade");
+    });
+
+    //Hamburger Animation
+    hamburger.classList.toggle("toggle");
+});
+
+document.getElementById('loginbutton').onclick = function () {
+    window.location.href = 'LoginPage.html';
+};
+
 var maxItems = 20;
 var initItemAmount = 10; //this is the intial amount of input rows created in the HTML
 var buttonAmount = 1;
